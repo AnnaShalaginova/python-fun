12903b4c24e1f82c62495c1fdabe8266149f7a2a
AnnaShalaginova <anya.shalaginova@gmail.com>
2020-01-09 18:35:09 -0500

    Update Python_Challenges.py

--- Python_Challenges.py
+++ Python_Challenges.py
@@ -1,13 +1,17 @@
 # program to create all possible permutations from a given collection of distinct numbers.
 # Source: https://www.w3resource.com/python-exercises/basic/python-basic-1-exercise-12.php
 def permute(nums):
-  result_perms = [[]]
+  empty_list = []
+  result_perms = [empty_list]
   for n in nums:
     new_perms = []
     for perm in result_perms:
       for i in range(len(perm)+1):
         new_perms.append(perm[:i] + [n] + perm[i:])
+        print(new_perms)
         result_perms = new_perms
+        print(result_perms)
+        print("")
   return result_perms
 
 my_nums = [1,2,3]
