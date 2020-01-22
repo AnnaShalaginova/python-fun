27b336d6881fcc6aa64fbf26e28d2c38196f59f6
AnnaShalaginova <anya.shalaginova@gmail.com>
2020-01-22 15:12:20 -0500

    Determine whether a number is even or odd

--- Python_Challenges.py
+++ Python_Challenges.py
@@ -17,3 +17,14 @@ def permute(nums):
 my_nums = [1,2,3]
 print("Original Cofllection: ",my_nums)
 print("Collection of distinct numbers:\n",permute(my_nums))
+
+#Determine if a number is even or odd
+#File name: even_or_odd.py
+
+number = input("Enter a number, and I'll tell you if it's even or odd: ")
+number = int(number)
+
+if number % 2 == 0:
+  print(f"\nThe number (number) is even.")
+else:
+  print(f"\nThe number {number} is odd.")
