f6c394b6cc2fdb8fe821b72878efd0ce5ec4c5d1
AnnaShalaginova <anya.shalaginova@gmail.com>
2020-02-05 16:04:10 -0500

    Using break to Exit a Loop
    Example with cities visited

--- Python_Challenges.py
+++ Python_Challenges.py
@@ -49,3 +49,16 @@ while active:
         active = False
     else: 
         print(message)
+      
+      
+#Using break to Exit a Loop
+prompt = "\nPlease enter the name of a city you have visited:"
+prompt += "n\Enter 'quit' to end the program. "
+while True: 
+    city = input(prompt)
+    
+    if city == 'quit':
+        break
+    else: 
+        print(f"I'd love to go to {city.title()}!")
+
