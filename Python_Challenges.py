dc0dfe9790e0478ef13f42347e93f7ac374dd1c1
AnnaShalaginova <anya.shalaginova@gmail.com>
2020-02-15 11:11:30 -0500

    Returning a simple value
    Take first and last name and return a formatted full name

--- Python_Challenges.py
+++ Python_Challenges.py
@@ -122,3 +122,12 @@ def describe_pet(pet_name, animal_type = 'dog'):
     print(f"\nI have a {animal_type}.")
     print(f"My {animal_type}'s name is {pet_name.title()}.")
 describe_pet(pet_name='willie')
+
+#formatted_name.py
+#Returning a simple value - take first and last name and return a formatted full name
+def get_formatted_name(first_name, last_name):
+    """Return a full name, neatly formatted."""
+    full_name = f"{first_name} {last_name}"
+    return full_name.title()
+musician = get_formatted_name('jimi','hendrix')
+print(musician)
