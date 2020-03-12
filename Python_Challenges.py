b2e33a04c3ace4003203d94ed161f9605518ea70
AnnaShalaginova <anya.shalaginova@gmail.com>
2020-03-12 15:37:14 -0400

    Running unit tests
    A test case is made of a number of unit tests to test behaviours of a function

--- Python_Challenges.py
+++ Python_Challenges.py
@@ -218,3 +218,43 @@ print(my_new_car.get_descriptive_name())
 
 my_new_car.odometer_reading = 23
 my_new_car.read_odometer()
+
+# name_function.py 
+
+def get_formatted_name(first, last): 
+    """Generate a neatly formatted full name."""
+    full_name = f"{first} {last}"
+    return full_name.title()
+
+# names.py
+
+# from name_function import get_formatted_name
+
+print("Enter 'q' at any time to quit.")
+while True: 
+    first = input("\nPlease give me a first name: ")
+    if first == 'q':
+        break
+    last = input("Please give me a last name: ")
+    if last == 'q':
+        break
+        
+    formatted_name = get_formatted_name(first, last)
+    print(f"\tNeatly formatted name: {formatted_name}.")
+    
+# test_name_function.py
+
+import unittest 
+
+from name_function import get_formatted_name
+
+class NamesTestCase(unittest.TestCase):
+    """Tests for 'name_function.py'."""
+    
+    def test_first_last_name(self):
+        """Do names like 'Janis Joplin' work?"""
+        formatted_name = get_formatted_name('janis', 'joplin')
+        self.assertEqual(formatted_name, 'Janis Joplin')
+        
+if __name__ == '__main__':
+    unittest.main()
